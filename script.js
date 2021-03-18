2ca0c4ed8b0cc4449e44e83765c012a2eb0d5885
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-18 17:33:26 +0000

    fixed errors.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.getElementById('change-image') .addEventListener('click', image)
-document.getElementById('change-text') .addEventListener('click', text)
-document.getElementById('change-hide') .addEventListener('click', hide)
-document.getElementById('change-show') .addEventListener('click', show)
-document.getElementById('change-bg') .addEventListener('click', background)
+document.getElementById('change-image').addEventListener('click', image)
+document.getElementById('change-text').addEventListener('click', text)
+document.getElementById('change-hide').addEventListener('click', hide)
+document.getElementById('change-show').addEventListener('click', show)
+document.getElementById('change-bg').addEventListener('click', background)
 
 function image () {
-  document.getElementById('picture').src = "images/Chihuahua.jpg"
+  document.getElementById('picture').src = 'images/Chihuahua.jpg'
 }
 
 function text () {
-  document.getElementById('text').innerHTML = "Hi I am a Chihuahua.And my name is chimchim."
+  document.getElementById('text').innerHTML = 'Hi I am a Chihuahua.And my name is chimchim.'
 }
 
 function hide () {
-  document.getElementById('text').style.display = "none"
+  document.getElementById('text').style.display = 'none'
 }
 
 function show () {
-  document.getElementById('text').style.display = "block"
+  document.getElementById('text').style.display = 'block'
 }
 
 function background () {
-  document.body.style.backgroundColor = "cadetblue"
+  document.body.style.backgroundColor = 'cadetblue'
 }
